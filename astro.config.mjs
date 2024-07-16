ab56d26aa6885f2d961cef490abaa52eeb13aaff
Long Kim Hoang <longkimhoang@outlook.com>
2024-07-17 01:09:24 +0700

    feat: change theme toggle to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
@@ -6,7 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://longkh.dev",
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: { enabled: true },
